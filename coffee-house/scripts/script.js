3d4b8b2ce9d5f062d0cb7b96bc3f690643ff0269
Sergey <seleznev-krista@mail.ru>
2023-12-02 16:53:44 +0300

    feat: add tablet adaptive style for home page

--- coffee-house/scripts/script.js
+++ coffee-house/scripts/script.js
@@ -1,4 +1,6 @@
 import {product} from './product.js';
+//import test from './test.json';
+//const readFile = fs.readFileSync('jsonex.json', 'utf8');
 import {ProductList} from './productListCreate.js';
 //import {PagesAtributes} from './buttonsBlock.js';
 import {ButtonsList} from './buttonsBlock.js'
@@ -6,7 +8,7 @@ import {PagesInterface} from './pagesInterface.js'
 
 const productList = new ProductList(product);
 const menuPage =  document.getElementById('menu-page');
-console.log(productList)
+//console.log(productList)
 
 menuPage.querySelector('.container').append(productList.listItems);
 
@@ -14,4 +16,30 @@ const pages = new PagesInterface(document.querySelectorAll('main'));
 
 const buttons = new ButtonsList(document.querySelectorAll('[data-page]'), pages);
 
+let  productListTest;
+
+function getFile (fileName) {
+
+    const request = new XMLHttpRequest();
+    request.open('get', fileName);
+    request.onloadend = function() {
+         parse(request.responseText);
+    }
+    
+
+    request.send();
+}
+
+getFile('./scripts/test.json'); //путь к файлу
+
+function parse(obj) {
+
+    productListTest = JSON.parse(obj);
+
+    console.log(productListTest);
+}
+
+// const test = getElementById('test')
+// const svg =  test.getSVGDocument();
+// console.log(svg)
 
