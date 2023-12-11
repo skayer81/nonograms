655284fa68ed5fa20950fd8744876590c362ae7c
Sergey <seleznev-krista@mail.ru>
2023-12-11 18:06:01 +0300

    feat: add load items button

--- coffee-house/scripts/baseComponentCreate/createComponent.js
+++ coffee-house/scripts/baseComponentCreate/createComponent.js
@@ -9,7 +9,7 @@ export class CreateBaseComponent{
      */
     createBaseComponent(parent, tag, classes = [], innerHTML = '') {
         let result = document.createElement(tag);
-        if (classes) result.classList.add(...classes);
+        if (classes.length > 0) result.classList.add(...classes);
         if (parent) parent.append(result);
         if (innerHTML) result.innerHTML = innerHTML;
         return result;
--- coffee-house/scripts/menuList/productListCreate.js
+++ coffee-house/scripts/menuList/productListCreate.js
@@ -4,24 +4,41 @@ import {ProductItem} from './createItem.js';
 import {itemPopUp} from '../menuPopup/itemsPopup.js';
 
 export class ProductList extends CreateBaseComponent{
+    HIDDEN_ITEM = 'hidden_Item';
     constructor(product){
         super();
         this._product = product;
+        this._hiddenItemList = []
     }
 
     getListItems(newCategory){
         return this._createMenu(newCategory);
     }
 
+    get hiddenItemList(){
+        return this._hiddenItemList
+    }
+
+    get hiddenClass(){
+        return this.HIDDEN_ITEM;
+    }
+
     _createMenu(newCategory){
+        this._hiddenItemList = []
         const list = this.createBaseComponent(0, 'ul', ['menu-items'])
         const ListOfCategory = this._initListOfCategory(newCategory);
+        let count = 1;
         for (let elem of ListOfCategory){
             let item = new ProductItem(elem.img, elem.name, elem.description, elem.price, newCategory).item;
             item.addEventListener('click',  () => {
                 itemPopUp.popupInit(elem);
             })
-            list.append(item)
+            if (count > 4) {
+                item.classList.add(this.HIDDEN_ITEM);
+                this._hiddenItemList.push(item);
+            }
+            list.append(item);
+            count += 1;
         }
         return list
     }
